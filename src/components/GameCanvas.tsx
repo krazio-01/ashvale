075debed4c1b16e584235c13e1a628f3a1ec3f0e
krazio-01 <md.krazio@gmail.com>
2026-08-13 10:07:40 +0530

    replace old scaffold with World-driven Scene, removing old components

--- src/components/GameCanvas.tsx
+++ src/components/GameCanvas.tsx
@@ -1,7 +1,7 @@
 "use client";
 import dynamic from "next/dynamic";
 
-const Scene = dynamic(() => import("./scene/Scene"), { ssr: false });
+const Scene = dynamic(() => import("./Scene"), { ssr: false });
 
 export default function GameCanvas() {
     return <Scene />;
